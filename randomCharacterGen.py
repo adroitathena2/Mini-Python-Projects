2d7f3b67f2cfd32f12fb52cc8fe7e38886363b50
Archisha Yadav <143172958+adroitathena2@users.noreply.github.com>
2025-06-16 14:46:12 +0530

    Create randomCharacterGen.py

---
+++ randomCharacterGen.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+lineWidth = 120
+curLines = {}
+chars = ['#', '*', '&', '$', '%', '@']
+
+for i in range(90000):
+  for j in rnage(lineWidth):
+    a = False
+    if j in curLines:
+      print(chars[random.randint(0, len(chars-1), end = "")
+      a = True
+      curLines[] -=1
+      
+      if curLines[j] == 0:
+        del curLines[j]
+    elif random.randint(0, 240) == 0:
+      curLines[j] = random.randint(0,50)
+
+    if not a:
+      print(" ", end = "")
+  print()
+  time.sleep(0.02)
+      
+        
